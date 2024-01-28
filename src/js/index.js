350e93e2cd6051889809b289281532a41a058098
BenitesALB <56932758+BenitesALB@users.noreply.github.com>
2024-01-28 18:25:28 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,18 @@
+import { calculate } from "./calculate.js"
+import copyToCLipboard from "./copyToCLipboard.js"
+import { handleButtonPress, handleClearButton, handleTyping } from "./keyHandlers.js"
+import themeSwitcher from "./themeSwitcher.js"
+
+document.querySelectorAll(".charKey").forEach(function (charKeyBtn) {
+  charKeyBtn.addEventListener("click", handleButtonPress)
+})
+
+document.getElementById("clear").addEventListener("click", handleClearButton)
+
+document.getElementById('input').addEventListener("keydown", handleTyping)
+
+document.getElementById("equal").addEventListener("click", calculate)
+
+document.getElementById("copyToClipboard").addEventListener("click", copyToCLipboard)
+
+document.getElementById("themeSwitcher").addEventListener("click", themeSwitcher)
